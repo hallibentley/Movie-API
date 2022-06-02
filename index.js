7d22d20b7f23dbb9055ab5290083b8c882170f76
hallibentley <hallibentley@gmail.com>
2022-06-02 14:32:11 -0400

    readded passport to /movies GET request

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get("/documentation", (req, res) => {
 });
 
 //Request for all movies//
-app.get("/movies", function (req, res) {
+app.get("/movies", passport.authenticate('jwt', { session: false }), function (req, res) {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
