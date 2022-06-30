c22eda7d8db15f9c53e4bc0c6fbcd2af624a6436
hallibentley <hallibentley@gmail.com>
2022-06-30 11:05:20 -0400

    Added cors allowed origins

--- index.js
+++ index.js
@@ -23,6 +23,24 @@ app.use(morgan("common"));
 
 const cors = require('cors');
 app.use(cors());
+
+let allowedOrigins = [
+  'http://localhost:8080',
+  'https://hallibentley-movie-api.herokuapp.com',
+  'https://hallibentley-myflix.netlify.app/'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
